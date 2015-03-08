3fcc0bce1fd1733b455e9db5167792e0f0a7ef6f
Ana Luczynski <ana@akta.com>
2015-03-08 15:48:49 -0500

    colors now set well, positioning fixed
    colors set according to click position
    colors set for both time and canvas
    time is positioned correctly
    window resize, canvas resizes and time repositions

--- src/events.js
+++ src/events.js
@@ -1,5 +1,6 @@
 (function() {
     'use strict';
+    var timeIsActive = false;
 
     window.addEventListener('click', handleClick, false);
     window.addEventListener('touchstart', handleTouch, false);
@@ -14,12 +15,26 @@
 
 
     function handleClick(e) {
+        console.log('clicked on the canvas!');
+        var x = e.clientX;
+        var y = e.clientY;
+
+        var what = setColor(y);
+        if (timeIsActive) {
+            console.log('canvas changed time color');
+            color.setOther(what, x);
+            deactivateTime();
+        } else {
+            color.set(what, x);
+        }
 
         e.preventDefault();
     }
     function handleTouch(e) {}
 
     function handleTimeClick(e) {
+        console.log('clicked on the time!');
+        activateTime();
         console.log(e);
 
         e.preventDefault();
@@ -29,5 +44,28 @@
         console.log(e);
         
         e.stopPropagation();
+    }
+
+    function setColor(y) {
+        var check = y / elm.height;
+        var which;
+        if (check > 2 / 3) {
+            which = 'l';
+        } else if (check > 1 / 3 && check <= 2 / 3) {
+            which = 's';
+        } else {
+            which = 'h';
+        }
+        return which;
+    }
+
+    function deactivateTime() {
+        timeIsActive = false;
+        elm.dom.time.classList.remove('active');
+    }
+
+    function activateTime() {
+        timeIsActive = true;
+        elm.dom.time.classList.add('active');
     }
 })();
--- src/loader.js
+++ src/loader.js
@@ -1,36 +1,59 @@
 (function() {
     'use strict';
+    document.addEventListener('DOMContentLoaded', _init, false);
 
     window.sizeCanvas = function() {
+        // console.log('resized!');
+        elm.height = window.innerHeight;
+        elm.width = window.innerWidth;
+
         elm.dom.canvas.height = elm.height;
         elm.dom.canvas.width = elm.width;
 
         elm.dom.time.style.top = elm.height / 2 + 'px';
+        elm.dom.time.style.left = elm.width / 2 - elm.dom.time.offsetWidth / 2 + 'px';
     };
+    window.addEventListener('resize', sizeCanvas, false);
 
-    // first set up the size of the canvas
-    sizeCanvas();
-
-    // create a global context variable
-    window.canvas = elm.dom.canvas.getContext('2d');
-
-    // set up the time
-    setTime(elm.storage.start);
-
-    var tilThen = 60 - elm.storage.start.getSeconds();
     
-    var wait = window.setTimeout(startUpdate, tilThen * 1000);
 
     //////
 
 
 
+    function _init() {
+        // console.log('ready!');
+        // clean up
+        document.removeEventListener('DOMContentLoaded', _init, false);
 
+        // first set up the size of the canvas
+        sizeCanvas();
+        // run it twice to give the resizing a chance
+        setTimeout(sizeCanvas, 100);
+
+        // create a global context variable
+        window.canvas = elm.dom.canvas.getContext('2d');
+
+        // set up the time
+        setTime(elm.storage.start);
+
+        var tilThen = 60 - elm.storage.start.getSeconds();
+        
+        var wait = window.setTimeout(startUpdate, tilThen * 1000);
+
+        // set up our color
+        // color.init();
+    }
 
     function setTime(input) {
         var hours = input.getHours();
-        var xm = (hours > 12) ? 'pm' : 'am';
-        hours = (hours > 12) ? hours - 12 : hours;
+        var xm = (hours > 11) ? 'pm' : 'am';
+        //how to deal with 12:00AM?
+        if (hours > 12) {
+            hours -= 12;
+        } else if (hours === 0) {
+            hours = String(12);
+        }
 
         var minutes = input.getMinutes();
 
